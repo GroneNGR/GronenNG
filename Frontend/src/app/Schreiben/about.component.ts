bcab215501a1fc4e4e56fe1222a8062a13708434
Anwender <riccardo77@t-online.de>
2018-06-21 12:29:52 +0200

    NESW

--- Frontend/src/app/Schreiben/about.component.ts
+++ Frontend/src/app/Schreiben/about.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router'
 import { DataService } from '../data.service';
+
+
+
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { MatDatepickerModule } from '@angular/material/datepicker';
+//import { MatNativeDateModule } from '@angular/material';
+//import { MatInputModule } from '@angular/material/input'
 import { DataService } from './data.service';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './speichern/home.component';
 import { AboutComponent } from './schreiben/about.component';
@@ -20,7 +23,10 @@ import { AboutComponent } from './schreiben/about.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    //MatDatepickerModule,
+    //MatNativeDateModule,
+    //MatInputModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
